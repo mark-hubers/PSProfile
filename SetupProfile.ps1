99584f1b74eb301429820a2464fc5f458e53c5b1
mhubers <mark.hubers@hubers-lab.com>
2013-08-11 20:25:34 -0500

    oo

--- SetupProfile.ps1
+++ SetupProfile.ps1
@@ -1,2 +1,29 @@
-write-host "Hi world"
-# https://github.com/mhubers/MgH_PS_Profile/raw/master/SetupProfile.ps1
+###
+### https://github.com/mhubers/PSProfile/raw/master/SetupProfile.ps1
+### (new-object Net.WebClient).DownloadString("https://github.com/mhubers/PSProfile/raw/master/SetupProfile.ps1") | iex 
+###
+
+    write-host "---------------------------------------------"
+    write-host "----      Welcome to profile setup       ----"
+    write-host "---------------------------------------------`n"
+
+    write-host "-Test execution policy..."
+    $exePolicy = (Get-ExecutionPolicy)
+    if ($exePolicy -eq "Restricted"){
+        Write-Warning "Your execution policy is $executionPolicy, this means you will not be able to use any script files."
+        Write-Warning "To fix this change your execution policy to something like RemoteSigned or Bypass."
+        Write-Warning "`n  Example: PS> Set-ExecutionPolicy RemoteSigned"
+        Write-Warning "`nOr`n  Example: PS> Set-ExecutionPolicy Bypass`n"
+    }
+
+
+
+### Test if existing profile or path exists.  If so we ask user if we want to overwrite the profile.
+if (Test-Path $profile ) {
+}   
+
+
+# https://github.com/mhubers/PSProfile/raw/master/SetupProfile.ps1
+
+Split-Path -
+New-Item -ItemType directory -Path C:\Scripts\newDir
