d5d75f3ff7f2adde1ab711c458b37822d00a07b3
mhubers <mark.hubers@hubers-lab.com>
2013-08-12 22:06:33 -0500

    oo

---
+++ Common_Alias.ps1
@@ -0,0 +1,22 @@
+##==============================================================================
+##
+##  DESCRIPTION...:  This is my profile aliases.
+##  AUTHOR........:  Mark Hubers
+##
+##==============================================================================
+	
+    
+function Alias:ProfileDisplayMessage {
+    write-host "Common Alias loaded into your environment."
+}
+
+	 ### Make aliases to notepad++
+	 if ( path-test "${Env:ProgramFiles(x86)}"
+	 
+    Set-Alias whoami Ask-Who
+    new-alias grep1 findstr
+	new-alias grep select-string
+    Set-Alias ed      "C:\Program Files\Notepad++\notepad++.exe"
+    Set-Alias edit    "C:\Program Files\Notepad++\notepad++.exe"
+
+
