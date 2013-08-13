907d6cf8f7d4b97235094236f8725255ff39ad33
mhubers <mark.hubers@hubers-lab.com>
2013-08-13 00:22:14 -0500

    oo

--- Common_Alias.ps1
+++ Common_Alias.ps1
@@ -10,15 +10,18 @@ function Alias:ProfileDisplayMessage {
     write-host "Common Alias loaded into your environment."
 }
 
-	 ### Make aliases to notepad++
-	 if ( path-test "${Env:ProgramFiles(x86)}" ) {
-
-}
+    ### Make aliases to notepad++
+    if ( Test-Path "${Env:ProgramFiles(x86)}" ) {
+        Set-Alias ed      "${Env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
+        Set-Alias edit    "${Env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
+    } else {
+        Set-Alias ed      "C:\Program Files\Notepad++\notepad++.exe"
+        Set-Alias edit    "C:\Program Files\Notepad++\notepad++.exe"
+    }
 	 
     Set-Alias whoami Ask-Who
     new-alias grep1 findstr
 	new-alias grep select-string
-    Set-Alias ed      "C:\Program Files\Notepad++\notepad++.exe"
-    Set-Alias edit    "C:\Program Files\Notepad++\notepad++.exe"
+
 
 
